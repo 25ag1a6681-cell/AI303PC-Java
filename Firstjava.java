6f63241035d38db5d74d0354ad4a9e48f9bb4fa1
25ag1a6681-cell <25ag1a6681@gmail.com>
2026-07-17 05:22:58 +0000

    FIrst LAB Program

---
+++ Firstjava.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Firstjava{
+    public static void main(String...args){
+          Scanner a = new Scanner(System.in);
+          IO.println("Enter a number:");
+          int x = a.nextInt();
+          IO.println("Enter a number:");
+          int y = a.nextInt();
+          var check = Math.random();
+          IO.println(check);
+    }
+}
